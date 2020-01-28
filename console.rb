5a27d7a1e9f7056c05dc450c2f7e51a4b7aebc71
mholwill <m_holwill@hotmail.co.uk>
2020-01-28 15:05:24 +0000

    Initial Commit

---
+++ console.rb
@@ -0,0 +1,18 @@
+require("pry-byebug")
+reqiure_relative("models/properties")
+
+property1 = PropertyTracker.new({
+  "address" => "60 Castle Street",
+  "value" => "£500000",
+  "number_of_bedrooms" => "3",
+  "year_built"=> "1892"
+  })
+property1.save()
+
+property2 = PropertyTracker.new({
+  "address" => "34 Leith Street",
+  "value" => "£200000",
+  "number_of_bedrooms" => "2",
+  "year_built"=> "1902"
+  })
+property2.save()
---
+++ models/properties.rb
@@ -0,0 +1,12 @@
+require("pg")
+
+class PropertyTracker
+
+  def initialize(options)
+    @id = options["id"].to_i() if options["id"]
+    @address = options["address"]
+    @value = options["value"]
+    @number_of_bedrooms = options["number_of_bedrooms"].to_i()
+    @year_built = options["year_built"].to_i()
+
+  end
